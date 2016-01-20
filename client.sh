391ef0ffb56f8153a163235348d84e73ffbb5cac
codejuan <xh@decbug.com>
2016-01-20 20:14:48 +0800

    client

--- client.sh
+++ client.sh
@@ -1,5 +1,8 @@
 sudo echo "192.168.1.173 docker-hub.huawei.com"  >>  /etc/hosts
 
-sudo mkdir -p /etc/docker/certs.d/docker-hub.huawei.com:5000/
+path=/etc/docker/certs.d/docker-hub.huawei.com
 
-sudo scp g530@docker-hub.huawei.com:/registry/certs/domain.crt /etc/docker/certs.d/docker-hub.huawei.com:5000/ca.crt
+
+sudo mkdir -p $path
+
+sudo cp -f certs/domain.crt /etc/docker/certs.d/docker-hub.huawei.com/ca.crt
